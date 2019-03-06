c30e47a577c5ed9980c1f997fcff9d6ad271bbfd
Vishal Zambre <v.zambre@gmail.com>
2019-03-06 20:59:41 +0530

    Initial commit of fullstackpuppet-ntp

--- manifests/config.pp
+++ manifests/config.pp
@@ -5,13 +5,4 @@
 # @example
 #   include ntp::config
 class ntp::config inherits ntp {
-
-  file { '/etc/ntp.conf':
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => 0644,
-    content => template($module_name/ntp.conf.erb),
-  }
-
 }
